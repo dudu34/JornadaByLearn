e1e2b363fd5a4511f7535d1fd2f092d0b12f1eb8
dudu34 <68355936+dudu34@users.noreply.github.com>
2021-01-07 15:31:19 -0300

    Add files via upload

---
+++ codigo.py
@@ -0,0 +1,16 @@
+def calcular_media(notas):
+  quantidade = len(notas)
+  soma = sun(soma)
+  media = soma / quantidade
+  print('O aluno tirou',media)
+  verificar_aprovacao(media)
+
+  def verificar_aprovacao(media):
+      if media >= 6:
+        print('Aluno Aprovado :)')
+      else:
+        print('Aluno Reprovado :(')
+
+        calcular_media = ([10, 8, 9, 7])
+  
+
